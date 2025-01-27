14b6b39f08ee47a870aa60ddbffe267f614a51e2
avyunusemre <deu.av.yunusemre@gmail.com>
2025-01-27 13:29:44 +0100

    websocket

--- src/main/java/com/yunuskaya/task/management/api/controllers/TaskWebSocketController.java
+++ src/main/java/com/yunuskaya/task/management/api/controllers/TaskWebSocketController.java
@@ -1,15 +1,22 @@
 package com.yunuskaya.task.management.api.controllers;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TaskWebSocketController {
 
-    @MessageMapping("/task-updates")
-    @SendToUser("/queue/reply")
-    public String handleTaskUpdate(String message) {
-        return "Task Update: " + message;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public TaskWebSocketController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/public")
+    public String broadcastMessage(String message) {
+        return message;
     }
 }
